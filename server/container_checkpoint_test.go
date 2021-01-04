4413eda798f9ec085509b9a258c159ef0399245b
Adrian Reber <areber@redhat.com>
2021-01-04 16:36:37 +0000

    checkpoint: export dns config in pod checkpoints
    Checkpoint a Pod with custom DNS config set fails during restore as CRIU
    tries to mount /etc/resolv.conf which points to the previous
    infrastructure container. Exporting all DNS config options in the Pod
    checkpoint enables CRI-O to restore the infrastructure container with
    the correct DNS settings and allows CRIU to restore the containers using
    that DNS setup.
    
    Signed-off-by: Adrian Reber <areber@redhat.com>

--- server/container_checkpoint_test.go
+++ server/container_checkpoint_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/containers/podman/v3/pkg/criu"
 	cstorage "github.com/containers/storage"
 	"github.com/containers/storage/pkg/archive"
+	"github.com/cri-o/cri-o/internal/hostport"
 	"github.com/cri-o/cri-o/internal/oci"
 	"github.com/cri-o/cri-o/pkg/private"
 	"github.com/golang/mock/gomock"
@@ -15,6 +16,7 @@ import (
 	specs "github.com/opencontainers/runtime-spec/specs-go"
 	"github.com/pkg/errors"
 	"google.golang.org/grpc/status"
+	types "k8s.io/cri-api/pkg/apis/runtime/v1"
 )
 
 var _ = t.Describe("ContainerCheckpoint", func() {
@@ -103,6 +105,59 @@ var _ = t.Describe("ContainerCheckpoint", func() {
 				State: specs.State{Status: oci.ContainerStateRunning},
 			})
 			testContainer.SetSpec(&specs.Spec{Version: "1.0.0"})
+
+			gomock.InOrder(
+				storeMock.EXPECT().Container(gomock.Any()).Return(&cstorage.Container{}, nil),
+				storeMock.EXPECT().Changes(gomock.Any(), gomock.Any()).Return([]archive.Change{}, nil),
+				imageServerMock.EXPECT().GetStore().Return(storeMock),
+				storeMock.EXPECT().Mount(gomock.Any(), gomock.Any()).Return("/tmp/", nil),
+				runtimeServerMock.EXPECT().StopContainer(gomock.Any()).Return(nil),
+			)
+			// When
+			err := sut.CheckpointContainer(context.Background(),
+				&private.CheckpointContainerRequest{
+					Id: testSandbox.ID(),
+					Options: &private.CheckpointContainerOptions{
+						CommonOptions: &private.CheckpointRestoreOptions{
+							Archive: "cp.tar",
+						},
+					},
+				})
+
+			// Then
+			Expect(err).To(BeNil())
+		})
+		It("should succeed with valid pod id and archive and DNSConfig and PortMapping", func() {
+			// Given
+			addContainerAndSandbox()
+
+			testContainer.SetState(&oci.ContainerState{
+				State: specs.State{Status: oci.ContainerStateRunning},
+			})
+			testContainer.SetSpec(&specs.Spec{Version: "1.0.0"})
+			testSandbox.SetDNSConfig(&types.DNSConfig{
+				Servers:  []string{"server1", "server2"},
+				Searches: []string{"searche1", "searches"},
+				Options:  []string{"option1", "option2"},
+			})
+			testSandbox.SetPortMappings([]*hostport.PortMapping{
+				{
+					ContainerPort: 2222,
+					HostPort:      1222,
+					Protocol:      "TCP",
+				},
+				{
+					ContainerPort: 2222,
+					HostPort:      1223,
+					Protocol:      "UDP",
+				},
+				{
+					ContainerPort: 2222,
+					HostIP:        "127.0.0.2",
+					HostPort:      1224,
+					Protocol:      "SCTP",
+				},
+			})
 
 			gomock.InOrder(
 				storeMock.EXPECT().Container(gomock.Any()).Return(&cstorage.Container{}, nil),
