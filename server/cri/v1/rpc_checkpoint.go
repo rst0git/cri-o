d555cf323a7bd88c75c3ba9b84ae9716a3b1622c
Adrian Reber <areber@redhat.com>
2022-03-10 13:05:12 +0000

    Wire through CRI API checkpoint/restore support
    Signed-off-by: Adrian Reber <areber@redhat.com>

---
+++ server/cri/v1/rpc_checkpoint.go
@@ -0,0 +1,24 @@
+package v1
+
+import (
+	"context"
+
+	"github.com/cri-o/cri-o/pkg/private"
+	pb "k8s.io/cri-api/pkg/apis/runtime/v1"
+)
+
+func (s *service) CheckpointContainer(ctx context.Context, req *pb.CheckpointContainerRequest) (*pb.CheckpointContainerResponse, error) {
+	request := &private.CheckpointContainerRequest {
+		Id: req.Id,
+		Options: &private.CheckpointContainerOptions{
+			CommonOptions: &private.CheckpointRestoreOptions{
+				Archive: req.Options.CommonOptions.ArchiveLocation,
+			},
+			LeaveRunning: req.Options.LeaveRunning,
+		},
+	}
+
+	result := s.server.CheckpointContainer(ctx, request)
+
+	return &pb.CheckpointContainerResponse{}, result
+}
